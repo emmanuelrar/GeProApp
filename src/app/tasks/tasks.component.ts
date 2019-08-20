f6f8dabb69e2e3b5c41b0bb4d868e036b0f0cd0e
Emmanuel Rasef Aguilar Rossi <emmanuel.aguilar@lesskomplex.com>
2019-08-20 12:13:31 -0400

    Configuracion de JSON-Server. Resta añadir los metodos httprequest.

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import * as lowdb from 'lowdb';
-import * as LocalStorage from 'lowdb/adapters/LocalStorage'
 import { FormGroup, FormControl } from '@angular/forms';
 
 export interface DialogData {
@@ -35,21 +33,13 @@ export class TasksComponent implements OnInit {
   constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
-    this.initDatabase();
   }
 
-  private async initDatabase() {
-    const adapter = new LocalStorage("db.json");
-    this.db = lowdb(adapter);
-  }
 
   AddNewTask() {
     this.dialog.open(AddTaskDialog, {
       width: '500px',
-      height: '80vh',
-      data: {
-        db: this.db
-      }
+      height: '80vh'
     });
   }
 
@@ -72,13 +62,5 @@ export class AddTaskDialog {
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
 
   CreateTask() {
-    console.log(this.newTaskForm.get('vencimiento').value)
-    this.data.db.get('posts')
-      .push({ 
-          task: this.newTaskForm.get('task').value
-        , detalle: this.newTaskForm.get('detalle').value
-        , vencimiento: this.newTaskForm.get('vencimiento').value 
-      })
-      .write()
   }
 }
